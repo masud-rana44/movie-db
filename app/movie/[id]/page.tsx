a8f30f8f0773623f95822508bcb953ab9755325f
masud-rana44 <mdmasudrana4488@gmail.com>
2024-10-26 19:02:55 +0600

    fix params issue on details page

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -14,7 +14,7 @@ export default async function MoviePage({
   params: { id: string };
 }) {
   try {
-    const movieId = parseInt(params.id);
+    const movieId = parseInt((await params).id, 10);
     const [movie, credits, recommendations] = await Promise.all([
       getMovieDetails(movieId),
       getMovieCredits(movieId),
